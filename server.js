08a8a84f1f58dac6d10d3fcc183501c7d0e628db
prjal-Bankoti <prajalbankoti971@gmail.com>
2021-12-14 17:14:37 +0530

    C:/Program Files/Git/

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 8000;
 
 server.use(middlewares)
 server.use(router)
-server.listen(3004, () => {
+server.listen(port, () => {
   console.log('JSON Server is running')
 })
